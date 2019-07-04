74b75f380d2de83eef2e70ec4f83288a448d584d
Ibrahim, Shehrman <shehrman.ibrahim@cgi.com>
2019-07-04 09:58:36 +0200

    Add lastname to model docktor

--- informaionUDB/Data/Migrations/20190704075145_InitialCreate1.Designer.cs
+++ informaionUDB/Data/Migrations/20190704075145_InitialCreate1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using informaionUDB.Data;
 
 namespace informaionUDB.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190704075145_InitialCreate1")]
+    partial class InitialCreate1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ informaionUDB/Data/Migrations/20190704075145_InitialCreate1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace informaionUDB.Data.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Docktors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Docktors");
+        }
+    }
+}
